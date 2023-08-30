93e6a72c275816bf86678393a73417b58b59748a
milesrichmond24 <miles.richmond24@d155.org>
2023-08-30 09:28:00 -0500

    Initial Commit

---
+++ mod1_creative_app_richmond/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  mod1_creative_app_richmond
+//
+//  Created by MILES RICHMOND on 8/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    func convert() {
+        return x;
+    }
+    
+}
+
