e0e779f701485b659c6c5178152d19d3ef01bd05
milesrichmond24 <miles.richmond24@d155.org>
2023-08-30 09:30:12 -0500

    Added a baller constant for no reason.

--- mod1_creative_app_richmond/ViewController.swift
+++ mod1_creative_app_richmond/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ball: Int;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
